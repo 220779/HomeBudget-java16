c481117095684961137d7aee7e9bf6a60c07a361
220779 <102145019+220779@users.noreply.github.com>
2022-11-05 10:11:58 +0200

    [change] - using @Bean for creation of Calculator bean

--- src/main/java/ee/secretagency/example/homebudgetjava16/HomeBudgetJava16Application.java
+++ src/main/java/ee/secretagency/example/homebudgetjava16/HomeBudgetJava16Application.java
@@ -1,7 +1,9 @@
 package ee.secretagency.example.homebudgetjava16;
 
+import ee.secretagency.endofthegame.homework.exercise1.Calculator;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class HomeBudgetJava16Application {
@@ -10,4 +12,11 @@ public class HomeBudgetJava16Application {
         SpringApplication.run(HomeBudgetJava16Application.class, args);
     }
 
+    //TODO: Explain
+    @Bean
+    public Calculator calculator(){
+        return new Calculator();
+    }
 }
+
+
