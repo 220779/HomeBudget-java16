6d668599f0aac3166b9e65a5c94167c0c9a0dc11
220779 <102145019+220779@users.noreply.github.com>
2022-11-21 07:15:44 +0200

    [refactor]- using one place to restoring urls

--- frontend/src/app/component/expense-list/expense-list.component.ts
+++ frontend/src/app/component/expense-list/expense-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { expenseListUrl, homeUrl, incomeListUrl} from "../../models/urls";
 
 @Component({
   selector: 'app-expense-list',
@@ -7,6 +8,9 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ExpenseListComponent implements OnInit {
 
+  home = homeUrl
+  expense = expenseListUrl
+  income = incomeListUrl
   constructor() { }
 
   ngOnInit(): void {
