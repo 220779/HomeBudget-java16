de54f5bccee01f329cb22f0a14d8a37b6ae7cd5e
220779 <102145019+220779@users.noreply.github.com>
2022-11-27 10:03:51 +0200

    [add]- calendar control added to the form

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -1,13 +1,12 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
 
-import { AppComponent } from './app.component';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { IncomeListComponent } from './components/income-list/income-list.component';
+import {AppComponent} from './app.component';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import {IncomeListComponent} from './components/income-list/income-list.component';
 import {HttpClientModule} from "@angular/common/http";
 import {MatCardModule} from "@angular/material/card";
 import {MatButtonModule} from "@angular/material/button";
-import { ExpenseListComponent } from './component/expense-list/expense-list.component';
 import {RouterModule} from "@angular/router";
 import { NotFoundComponent } from './components/not-found/not-found.component';
 import {MatIconModule} from "@angular/material/icon";
@@ -18,7 +17,10 @@ import {createNewIncomeUrl, expenseListUrl, homeUrl, incomeListUrl, notFoundUrl}
 import {MatFormFieldModule} from "@angular/material/form-field";
 import {MatSelectModule} from "@angular/material/select";
 import {MatRadioModule} from "@angular/material/radio";
-
+import {MatInputModule} from "@angular/material/input";
+import {MatDatepickerModule} from "@angular/material/datepicker";
+import {MatNativeDateModule} from "@angular/material/core";
+import {ExpenseListComponent} from "./component/expense-list/expense-list.component";
 
 @NgModule({
   declarations: [
@@ -47,9 +49,12 @@ import {MatRadioModule} from "@angular/material/radio";
     MatFormFieldModule,
     MatSelectModule,
     MatRadioModule,
-
+    MatInputModule,
+    MatDatepickerModule,
+    MatNativeDateModule
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
