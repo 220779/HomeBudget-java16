f7ab7f9ee27365a7637653f43b4b47ae6f1585f3
220779 <102145019+220779@users.noreply.github.com>
2022-12-02 09:20:05 +0200

    [fixing program errors]- back to connection-back-end branch

---
+++ frontend/src/app/services/income-service/income.service.ts
@@ -0,0 +1,26 @@
+import { Injectable } from '@angular/core';
+import {HttpClient} from "@angular/common/http";
+import {allIncomesUrl, newIncomeUrl} from "../../models/urls";
+import {Income} from "../../models/income";
+import {Observable} from "rxjs";
+
+@Injectable({
+  providedIn: 'root'
+})
+export class IncomeService {
+
+  //TODO: add module
+
+  constructor(private httpClient: HttpClient) { }
+
+  getAllIncomes(): Observable<Array<Income>> {
+    return this.httpClient.get<Array<Income>>(allIncomesUrl)
+
+
+  }
+
+  createNewIncome(income: { amount: number; person: string; creationTimestamp: null; currency: string; id: null; category: string; updateTimestamp: null; timestamp: string }) : Observable<Income>{
+    return this.httpClient.post<Income>(newIncomeUrl, income)
+  }
+}
+
